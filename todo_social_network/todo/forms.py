644e2a3de247f97490f85263d1f4978be157a75b
Heldash <dartin.rok@yandex.ru>
2025-02-12 01:06:04 +0300

    Добавил tailwindcss

--- todo_social_network/todo/forms.py
+++ todo_social_network/todo/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-
-
 class AddTodoForm(forms.Form):
-    text = forms.CharField(label="Название")
+    text_class="border-2 bg-neutral-300 transition rounded-sm p-2 pl-2 hover:bg-neutral-200 focus:bg-neutral-100 hover:border-blue-950 focus:scale-103 hover:scale-103"
+    text = forms.CharField(label="Название",widget=forms.TextInput(attrs={"class":text_class}))
     deadline = forms.DateField(label="Дедлайн",widget=forms.SelectDateWidget)
 class AddCommentForm(forms.Form):
-    text = forms.CharField(label="Комментарий",widget=forms.Textarea)
+    text_class="border-2 h-13 bg-neutral-300 transition rounded-sm p-2 pl-2 hover:bg-neutral-200 focus:bg-neutral-100 hover:border-blue-950 focus:border-3 hover:border-3"
+    text = forms.CharField(label="Комментарий",widget=forms.Textarea(attrs={"class":text_class}))
